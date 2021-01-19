f69835a0bfd9c5d1692b5d505f56be4b57f9a223
cys-mbp13 <yuans_c@icloud.com>
2021-01-19 17:41:28 +0800

    商品列表开发 上下拉刷新 请求toast

--- mallpractice-2/pages/goods_list/index.js
+++ mallpractice-2/pages/goods_list/index.js
@@ -1,65 +1,110 @@
+import { request } from "../../request/index.js"
+
 //Page Object
 Page({
   data: {
-    tabs:[
+    tabs: [
       {
-        id:0,
-        value:"综合",
-        isActive:true
+        id: 0,
+        value: "综合",
+        isActive: true
       },
       {
-        id:1,
-        value:"销量",
-        isActive:false
+        id: 1,
+        value: "销量",
+        isActive: false
       },
       {
-        id:2,
-        value:"价格",
-        isActive:false
+        id: 2,
+        value: "价格",
+        isActive: false
       }
     ],
-    index:0
+    index: 0,
+    goods_list:[],
+    totalPages:0
   },
-  handleTabsItemChange (e) {
+  // 搜索商品需要的参数
+  QueryParams:{
+    query:"",
+    cid:"",
+    pagenum:1,
+    pagesize:10
+  },
+
+  handleTabsItemChange(e) {
     console.log(e);
-    const {index} = e.detail;
-    const {tabs} = this.data;
-    tabs.forEach((v,i) => i===index?v.isActive=true:v.isActive=false);
+    const { index } = e.detail;
+    const { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
     this.setData({
       index,
       tabs
     })
   },
+
+  async getGoodsList () {
+    const res = await request({url:"/goods/search",data:this.QueryParams});
+    // 向上取整计算总页数 = 总条数 / 每页条数 
+    this.data.totalPages = Math.ceil(res.total/this.QueryParams.pagesize);
+    this.setData({
+      // 拼接数据
+      goods_list:[...this.data.goods_list,...res.goods]
+    })
+    // 停止下拉刷新
+    wx.stopPullDownRefresh;
+  },
+
   //options(Object)
-  onLoad: function(options){
-    
+  onLoad: function (options) {
+    this.QueryParams.cid = options.cat_id;
+    this.getGoodsList();
   },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
+
+  onReady: function () {
 
   },
-  onUnload: function(){
+  onShow: function () {
 
   },
-  onPullDownRefresh: function(){
+  onHide: function () {
 
   },
-  onReachBottom: function(){
+  onUnload: function () {
 
   },
-  onShareAppMessage: function(){
+  onPullDownRefresh: function () {
+    this.setData({
+      goods_list:[]
+    })
+    this.QueryParams.pagenum = 1;
+    this.data.totalPages = 0;
+    this.getGoodsList();
+  },
+  onReachBottom: function () {
 
   },
-  onPageScroll: function(){
+  onShareAppMessage: function () {
+
+  },
+  onPageScroll: function () {
 
   },
   //item(index,pagePath,text)
-  onTabItemTap:function(item){
+  onTabItemTap: function (item) {
 
+  },
+
+  // 页面上拉触底事件
+  onReachBottom() {
+
+    if (this.QueryParams.pagenum >= this.data.totalPages) {
+      // 加载完了
+      wx.showToast({title: '加载完了'});
+    } else {
+      // 加载
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
   }
 });
--- mallpractice-2/request/index.js
+++ mallpractice-2/request/index.js
@@ -1,4 +1,8 @@
+// 记录同时发送异步请求的次数
+let ajaxTimes = 0;
 export const request = (params) => {
+    ajaxTimes++;
+    wx.showLoading({title: '加载中',mask: true});
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve, reject) => {
         wx.request({
@@ -10,6 +14,12 @@ export const request = (params) => {
             },
             fail: (err) => {
                 reject(err);
+            },
+            complete:()=>{
+                ajaxTimes--;
+                if (ajaxTimes===0) {
+                    wx.hideLoading();
+                }
             }
         });
     })
