890bbac4891dc34b71501312ff212a839b2d5076
cys-mbp13 <yuans_c@icloud.com>
2021-01-20 17:43:53 +0800

    商品详情页开发

--- mallpractice-2/pages/goods_detail/index.js
+++ mallpractice-2/pages/goods_detail/index.js
@@ -1,66 +1,46 @@
-// pages/goods_detail/index.js
+//Page Object
+
+import { request } from "../../request/index.js"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsObj:{},
+    currentSwiperItemIndex:0
+  },
+  //options(Object)
+  onLoad: function(options){
+    const {goods_id} = options;
+    this.getGoodsDetail(goods_id);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  async getGoodsDetail (goods_id) {
+    const goodsObj = await request({url:"/goods/detail",data:{goods_id}});
+    this.setData({
+      goodsObj:{
+        pics:goodsObj.pics,
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        goods_introduce:goodsObj.goods_introduce
+        // iphone 部分手机不是别 webp 图片格式
+        // 最好跟后台协商改为 jpg
+        // 可以如下临时将 webp 改为 jpg
+        // goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg')
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleSwiperTap (e) {
+    console.log(e);
+    const urls = this.data.goodsObj.pics.map(v=>v.pics_mid);
+    const current = this.data.goodsObj.pics[this.data.currentSwiperItemIndex].pics_mid;
+    wx.previewImage({
+      current,
+      urls
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSwiperChange (e) {
+    console.log(e);
+    this.data.currentSwiperItemIndex = e.detail.current;
   }
-})
+});
