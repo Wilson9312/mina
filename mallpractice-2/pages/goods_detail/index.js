e12d863d591d7e529cfc4156bf3683e526e1193e
cys-mbp13 <yuans_c@icloud.com>
2021-01-26 11:47:39 +0800

    详情工具栏开发

--- mallpractice-2/pages/goods_detail/index.js
+++ mallpractice-2/pages/goods_detail/index.js
@@ -7,6 +7,9 @@ Page({
     goodsObj:{},
     currentSwiperItemIndex:0
   },
+  // data里放的是双向绑定的数据，即标签用到的数据，如果把没有用的数据放到data里，会损耗性能
+  GoodsInfo:{},
+
   //options(Object)
   onLoad: function(options){
     const {goods_id} = options;
@@ -15,8 +18,11 @@ Page({
 
   async getGoodsDetail (goods_id) {
     const goodsObj = await request({url:"/goods/detail",data:{goods_id}});
+    this.GoodsInfo = goodsObj;
+    console.log(this.GoodsInfo);
     this.setData({
       goodsObj:{
+        // 原数据有许多键值对，处于性能优化问题，这里复制必要数据
         pics:goodsObj.pics,
         goods_name:goodsObj.goods_name,
         goods_price:goodsObj.goods_price,
@@ -25,14 +31,14 @@ Page({
         // 最好跟后台协商改为 jpg
         // 可以如下临时将 webp 改为 jpg
         // goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg')
-      }
+      },
     })
   },
 
   handleSwiperTap (e) {
     console.log(e);
-    const urls = this.data.goodsObj.pics.map(v=>v.pics_mid);
-    const current = this.data.goodsObj.pics[this.data.currentSwiperItemIndex].pics_mid;
+    const urls = this.GoodsInfo.pics.map(v=>v.pics_mid);
+    const current = this.GoodsInfo.pics[this.data.currentSwiperItemIndex].pics_mid;
     wx.previewImage({
       current,
       urls
@@ -42,5 +48,12 @@ Page({
   handleSwiperChange (e) {
     console.log(e);
     this.data.currentSwiperItemIndex = e.detail.current;
+  },
+
+  handleAddCart (e) {
+    
+    let cartInfo = wx.getStorageSync('Cart') || [];
+    // cartInfo.map(v=>v.)
   }
 });
+
