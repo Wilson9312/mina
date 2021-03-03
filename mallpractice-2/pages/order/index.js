1eb3eb80f8e4b4ad76f63fe485eb46544f458228
cys-mbp13 <yuans_c@icloud.com>
2021-03-03 17:56:18 +0800

    订单查询开发

--- mallpractice-2/pages/order/index.js
+++ mallpractice-2/pages/order/index.js
@@ -1,66 +1,94 @@
 // pages/order/index.js
+import { request } from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待发货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      }
+    ],
+    index: 0,
+    orders: []
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    // onShow 不同于 onLoad，没有接收传过来的数据
 
+    // 0.1 读取缓存，看有没有token
+    const token = wx.getStorageSync("token");
+    if (!token) {
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return;
+    }
+
+    // 1.获取页面栈数组
+    const pages = getCurrentPages();
+    // 2.获取当前页
+    const curPage = pages[pages.length - 1];
+    // 3.获取当前页接收的数据
+    const pageOptions = curPage.options;
+    const type = pageOptions.type;
+    // 4.获取订单列表
+    this.getOrders(type);
+    this.changeSelectedTabsItem(type - 1);
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  // 获取订单列表的方法
+  async getOrders(type) {
+    const { orders } = await request({ url: "/my/orders/all", data: { type } });
 
+    this.setData({
+      // 时间格式化：中国标准时间
+      orders: orders.map(v => ({ ...v, create_time_cn: (new Date(v.create_time * 1000).toLocaleString()) }))
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  // 处理tab选中项
+  changeSelectedTabsItem(index) {
+    const { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      index,
+      tabs
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 处理tab点击
+  handleTabsItemChange(e) {
+    // 1.获取被点击的tab索引，并更新选中
+    const { index } = e.detail;
+    this.changeSelectedTabsItem(index);
+    // 2.重新发送请求
+    this.getOrders(index + 1);
   }
 })
